01e521dbafc71091841fe3560c97dc6e6ab9ba9e
Theodor-Alexandru Irimia <11174371+tirimia@users.noreply.github.com>
2024-05-05 22:03:51 +0200

    Populate flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "";
+  description = "Overlay for go cross-compilation using zig as a C toolchain";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -14,9 +14,36 @@
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = import nixpkgs {inherit system;};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            self.outputs.overlays.${system}.default
+          ];
+        };
       in {
         formatter = pkgs.alejandra;
+        overlays.default = import ./overlay.nix;
+        packages = let
+          pkgBuilder = {
+            GOOS,
+            GOARCH,
+          }:
+            pkgs.buildGoModule {
+              inherit GOOS GOARCH;
+              src = ./example;
+              vendorHash = "sha256-7hRezOBcjB2wsx/SwV519wg3Azh+0kHMcAoc9aYPM3A=";
+              name = "example";
+            };
+        in {
+          linuxExample = pkgBuilder {
+            GOOS = "linux";
+            GOARCH = "amd64";
+          };
+          macosExample = pkgBuilder {
+            GOOS = "darwin";
+            GOARCH = "arm64";
+          };
+        };
       }
     );
 }
